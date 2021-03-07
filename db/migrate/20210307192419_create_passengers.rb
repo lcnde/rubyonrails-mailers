1513a937633122a411722b70c5cc876170f76d29
lcnde <176673026+lcnde@users.noreply.github.com>
2021-03-07 20:24:53 +0100

    generated models

---
+++ db/migrate/20210307192419_create_passengers.rb
@@ -0,0 +1,11 @@
+class CreatePassengers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :passengers do |t|
+      t.text :name
+      t.text :surname
+      t.references :booking, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
