7f1d569629282d1830ba95a7564158b72d647b08
lcnde <176673026+lcnde@users.noreply.github.com>
2021-03-07 21:54:51 +0100

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "flights#index"
 
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create]
+  resources :passengers, only: [:new, :create]
+  
 end
