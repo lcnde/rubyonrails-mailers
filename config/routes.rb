5842e6717569d519afc087bbbd701429fcac7d43
lcnde <176673026+lcnde@users.noreply.github.com>
2021-03-07 21:57:28 +0100

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'passengers/new'
+  get 'passengers/create'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'flights/index'
   root "flights#index"
 
   resources :flights, only: [:index]
