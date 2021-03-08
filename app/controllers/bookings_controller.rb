2f917a82786b9d0fc146b9753cd1fc62b45e9501
lcnde <176673026+lcnde@users.noreply.github.com>
2021-03-08 16:16:00 +0100

    commit before nested attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,22 @@
 class BookingsController < ApplicationController
+  
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirecto_to flights_path
+    else
+      flash.now[:alert] = "Something went wrong"
+      render :new
+    end
   end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, :email, :billing, passenger: [:name, :surname, :booking_id])
+  end
+
 end
