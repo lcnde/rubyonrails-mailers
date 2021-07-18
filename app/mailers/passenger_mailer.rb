ff211c441e96774f5e2438e298ad8c817c22ff9d
lcnde <176673026+lcnde@users.noreply.github.com>
2021-07-18 14:44:32 +0200

    basic email controller

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def confirmation_email
+    @user = params[:user]
+    @url = 'https://example.com/login'
+    mail(to: @user.email, subject: 'Booking successfully received')
+  end
 end
